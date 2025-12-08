cde4760275275ee280d9c94fa9457f6b2155b130
EstebannHM <estebanh493@gmail.com>
2025-12-08 03:32:34 -0600

    Actualizacion datos en footer y contact.php

--- includes/footer.php
+++ includes/footer.php
@@ -20,32 +20,22 @@
     <div class="footer-col footer-contact">
       <h4>Contacto</h4>
       <ul class="contact-list">
-        <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+        <li><svg width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
-          </svg> +506 8418 6081</li>
-        <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+          </svg> +506 2230-8023</li>
+        <li><svg width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
             <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
-          </svg> <a href="mailto:info@medicalworks.com">info@medicalworks.com</a></li>
-        <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+          </svg> <a href="mailto:info@medworkcr.com">info@medworkcr.com</a></li>
+        <li><svg width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
             <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6" />
-          </svg> San José, Costa Rica</li>
+          </svg> San José, Aserrí</li>
       </ul>
 
       <div class="footer-social" aria-label="Síguenos">
 
-        <a href="#" aria-label="Facebook" class="social-icon" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.facebook.com/p/Medical-Works-Ltda-100063671643322/" aria-label="Facebook" class="social-icon" target="_blank" rel="noopener noreferrer">
           <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
             <path d="M22 12.07C22 6.48 17.52 2 11.93 2S2 6.48 2 12.07c0 4.99 3.66 9.13 8.44 9.94v-7.04H8.17v-2.9h2.27V9.41c0-2.24 1.33-3.48 3.36-3.48.97 0 1.98.17 1.98.17v2.18h-1.12c-1.1 0-1.44.68-1.44 1.38v1.66h2.45l-.39 2.9h-2.06V22C18.34 21.2 22 17.06 22 12.07z" />
-          </svg>
-        </a>
-        <a href="#" aria-label="Instagram" class="social-icon" target="_blank" rel="noopener noreferrer">
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-            <path d="M7 2C4.243 2 2 4.243 2 7v10c0 2.757 2.243 5 5 5h10c2.757 0 5-2.243 5-5V7c0-2.757-2.243-5-5-5H7zm0 2h10c1.654 0 3 1.346 3 3v10c0 1.654-1.346 3-3 3H7c-1.654 0-3-1.346-3-3V7c0-1.654 1.346-3 3-3zm5 3.5A4.5 4.5 0 1 0 16.5 12 4.505 4.505 0 0 0 12 7.5zm0 2A2.5 2.5 0 1 1 9.5 12 2.503 2.503 0 0 1 12 9.5zM18.5 6a.75.75 0 1 1-.75-.75A.75.75 0 0 1 18.5 6z" />
-          </svg>
-        </a>
-        <a href="#" aria-label="LinkedIn" class="social-icon" target="_blank" rel="noopener noreferrer">
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-            <path d="M19 2H5a3 3 0 0 0-3 3v14a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V5a3 3 0 0 0-3-3zM8.5 18.5H6V10h2.5v8.5zM7.25 8.75a1.25 1.25 0 1 1 0-2.5 1.25 1.25 0 0 1 0 2.5zM18 18.5h-2.5v-4.5c0-1.07-.02-2.45-1.5-2.45-1.5 0-1.73 1.17-1.73 2.38v4.57H10V10h2.4v1.16h.03c.33-.62 1.14-1.27 2.35-1.27 2.52 0 2.99 1.66 2.99 3.82v4.79z" />
           </svg>
         </a>
       </div>
